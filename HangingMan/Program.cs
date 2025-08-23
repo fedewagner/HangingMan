079c043621aa47cde68b3a20d32ed25d1c916772
fedewagner <federico.j.wagner@gmail.com>
2025-08-23 22:54:08 +0200

    final commit

--- HangingMan/Program.cs
+++ HangingMan/Program.cs
@@ -4,30 +4,83 @@
     {
         static void Main(string[] args)
         {
-            /*
-             * Hangman is a game where a word is secretly chosen and the player guesses letters to fill in the word.
-               Each correct guess fills in that letter in the word.  Guess too many wrong letters and the player loses.
-               Tips: It is all a matter of taking the letter the player guesses and looping through the word comparing it to each letter in the word character by character. If the letters match, you show that letter to the player. If you reach the end of the word and no letters have been matched, it is a wrong guess.
-               Remember that strings are often treated as an array of characters. Most languages have a function to fetch a single letter from a string.
-               Keep track of how many wrong guesses the player has done and use this number to determine if the game has been won or lost.
-               Added Difficulty: Increase the length of the words and choose more complex unknown words to have the player guess.
-             */
+            //Putting some art
+            var HANGMANPICS = new List<string>
+            {
+                @"
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========",
+                @"
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========",
+                @"
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========",
+                @"
+  +---+
+  |   |
+  O   |
+ /|\  |
+  |   |
+      |
+=========",
+                @"
+  +---+
+  |   |
+  O   |
+ /|\  |
+  |   |
+ / \  |
+========="
+            };
 
             //Define a List or Dictionary with words
-            //Select randomly one of those
-            //Print the screen telling the user to guess one character
-            //check if the character was already given?
-            //For each character in the word, check if that character equals with the one give by the user
-            //if the character equals, then add this to a list with the positive found characters
-            //if the character doesn't equal, then add a counter of tries ++1
-            //then check the word and compare with the all the elements from the positive list and in case, a character is found, then add that character to a variable
-            // if that character is not in the list, then add "_"
-            //Print the variable
-            //ask the user to enter a new character
-
-            //Define a List or Dictionary with words
-            List<string> listOfWords = new List<string>() { "hello", "dog", "house", "car" };
-            List<char> triedCharacters = new List<char>();
+            List<string> listOfWords = new List<string>()
+            {
+                "abundance", "accomplish", "adventure", "affectionate", "amusement",
+                "appearance", "appreciate", "astonishing", "atmosphere", "background",
+                "benevolent", "biological", "bookkeeper", "brilliance", "calculation",
+                "capability", "celebration", "characteristic", "civilization", "collaboration",
+                "commitment", "communication", "competition", "complexity", "composition",
+                "comprehensive", "consequence", "consideration", "construction", "contagious",
+                "convenience", "cooperation", "courageous", "creativity", "curiosity",
+                "dedication", "deliberate", "democratic", "description", "determination",
+                "development", "discovery", "distinction", "electricity", "enthusiasm",
+                "environment", "establishment", "evaluation", "experience", "explanation",
+                "fascination", "friendship", "fundamental", "generation", "governments",
+                "gratitude", "harmonious", "imagination", "importance", "improvement",
+                "independent", "information", "innovation", "instrument", "intelligence",
+                "interaction", "interesting", "intermediate", "investigate", "invitation",
+                "knowledgeable", "leadership", "librarianship", "literature", "magnificent",
+                "mathematical", "meaningful", "medication", "motivation", "multimedia",
+                "navigation", "necessities", "negotiation", "observation", "opportunity",
+                "organization", "partnership", "performance", "personality", "perspective",
+                "philosopher", "photography", "population", "possibility", "preparation",
+                "productivity", "professional", "psychology", "recognition", "relationship"
+            };
+            
+            //allowed alphabet
+            var alphabetLower = new List<char>
+            {
+                'a','b','c','d','e','f','g','h','i','j',
+                'k','l','m','n','o','p','q','r','s','t',
+                'u','v','w','x','y','z'
+            };
 
             //Select randomly one of those words
             Random random = new Random();
@@ -35,86 +88,125 @@
             int listLength = listOfWords.Count;
             int indexWordToGuess = random.Next(MIN_FOR_RANDOM_FUNCTION, listLength);
             string wordToGuess = listOfWords[indexWordToGuess];
-            //Console.WriteLine(wordToGuess);
 
-            //Welcome the user and print the screen telling the user to guess one character
+            //Welcome the user
             Console.WriteLine(
                 "Hi there! You need to pick one character to figure out which word has been picked! (only one!)");
 
-            //Variables and constants declaration
-            const int MAXTRIED_CHARACTERS = 4;
-            int triedCharactersCount = 0;
-            int amountOfDifferentCharactersToGuess = wordToGuess.Distinct().Count();
-            int guessedCharactersCount = 0;
+            //Variables, constants and lists declaration
+            const int MAXTRIEDCHARACTERS = 5;
 
-            //While Loop with tries counter
-            while (triedCharactersCount < MAXTRIED_CHARACTERS &&
-                   amountOfDifferentCharactersToGuess > guessedCharactersCount)
+            List<char> charactersToGuessList = new List<char>(wordToGuess);
+            List<char> triedCharacters = new List<char>();
+
+            int wrongtriedCharactersCount = 0;
+            int countOfCharactersToGuess = charactersToGuessList.Distinct().Count();
+            int triesLeft = MAXTRIEDCHARACTERS;
+
+
+            //While Loop with triedCharactersCount for checking loosing and countOfCharactersToGuess for winning
+            while (triesLeft > 0 &&
+                   countOfCharactersToGuess > 0)
             {
-                //Print the screen telling the user to guess one character
-                Console.WriteLine("What Character should we check next?");
-
-                //store user's character
-                string userCharacterGuess;
-                userCharacterGuess = Console.ReadLine().ToLower();
-                //ADITONAL: make sure only one character is entered
-                while (userCharacterGuess.Length != 1)
-                {
-                    Console.WriteLine("What Character should we check? Please enter ONLY one character.");
-                    userCharacterGuess = Console.ReadLine().ToLower();
+                //Print the screen for the user and read key
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("What character should be checked?");
+                Console.ForegroundColor = ConsoleColor.White;
+                
+                //check the introduced character is in the accepted dictionary
+                var keyInfo = Console.ReadKey(true);
+                char userCharacterGuess = char.ToLower(keyInfo.KeyChar);
+                while (!alphabetLower.Contains(userCharacterGuess)){
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("Please introduce a character from the english alphabet!");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    keyInfo = Console.ReadKey(true);
+                    userCharacterGuess = char.ToLower(keyInfo.KeyChar);
                 }
-
                 //check if the character was already given.
-                if (triedCharacters.Contains(userCharacterGuess[0]))
+                if (triedCharacters.Contains(userCharacterGuess))
                 {
-                    Console.WriteLine("That word is already picked!");
+                    //already used character => we don't do anything
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"You've already tried with '{userCharacterGuess}'!");
+                    Console.ForegroundColor = ConsoleColor.White;
                 }
                 else
                 {
-                    triedCharacters.Add(userCharacterGuess[0]);
-                    Console.WriteLine($"'{userCharacterGuess[0]}' just added");
-                }
+                    //the character is new, so we add it to the list of tried characters
+                    Console.WriteLine($"'{userCharacterGuess}' just checked");
+                    triedCharacters.Add(userCharacterGuess);
 
-                //check if the word contains the guessed character
-                if (wordToGuess.Contains(userCharacterGuess[0]))
-                {
-                    Console.WriteLine($"Well done! '{userCharacterGuess[0]}' is in the word!");
-                    guessedCharactersCount++;
-                }
-                else
-                {
-                    Console.WriteLine(
-                        $"The character is not in the word! Try again, you have still {MAXTRIED_CHARACTERS - triedCharactersCount}.");
-                    triedCharactersCount++;
-                }
-
-                //For each character in the word, check if that character equals with the one give by the user
-                foreach (char character in wordToGuess)
-                {
-                    if (triedCharacters.Contains(character))
+                    //check if the word contains the guessed character
+                    if (wordToGuess.Contains(userCharacterGuess))
                     {
-                        Console.Write(string.Concat(character, ' '));
+                        //in case it contains it ==> nicely done, we congratulate the user
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Well done! '{userCharacterGuess}' is in the word!");
+                        Console.ForegroundColor = ConsoleColor.White;
+                        
+                        //remove the all those characters from the list to be guessed
+                        charactersToGuessList.RemoveAll(c => c == userCharacterGuess);
+                        
+                        //count the characters that still need to be guessed
+                        countOfCharactersToGuess = charactersToGuessList.Distinct().Count();
                     }
                     else
                     {
-                        Console.Write(string.Concat('_', ' '));
+                        //in case it's not in the word => removes a "life"
+                        triesLeft--;
+                        //increase the counter for the art list
+                        wrongtriedCharactersCount++;
+                        
+                        //in case we still have some lifes available => print extra text and amount of lifes available
+                        if (triesLeft > 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(
+                                $"The character is not in the word! Try again, you have still {triesLeft}.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(HANGMANPICS[wrongtriedCharactersCount - 1]);
+                        }
                     }
-                    //Console.Write(character);
                 }
+                
+                // in case we still have some character to guess
+                if (countOfCharactersToGuess > 0 && triesLeft > 0)
+                {
+                    //Print the word with guessed characters
+                    Console.Write("The word with your guesses looks like this: ");
 
-                //Print empty line
-                Console.WriteLine();
+                    //For each character in the word, check if that character equals with the one give by the user
+                    foreach (char character in wordToGuess)
+                    {
+                        if (triedCharacters.Contains(character))
+                        {
+                            Console.Write(string.Concat(character, ' '));
+                        }
+                        else
+                        {
+                            Console.Write(string.Concat('_', ' '));
+                        }
+                    }
+
+                    //Print empty line
+                    Console.WriteLine();
+                }
             }
 
-
-            if (amountOfDifferentCharactersToGuess == guessedCharactersCount)
+            //final check if user won or not
+            if (countOfCharactersToGuess == 0)
             {
-                Console.WriteLine("Well done! You won 100$");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"You won 100$ guessing the word {wordToGuess}!");
+                Console.ForegroundColor = ConsoleColor.White;
             }
             else
             {
-                Console.WriteLine();
-                Console.WriteLine("I'm afraid you haven't guessed the word! ");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine("I'm afraid you haven't guessed the word! and you have no try left!");
+                Console.ForegroundColor = ConsoleColor.Yellow;
                 Console.WriteLine($"The word to guess was: {wordToGuess}");
             }
         }
